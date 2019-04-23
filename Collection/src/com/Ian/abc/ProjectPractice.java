ddb6580ab014d78d5591a3cf750be0971d879a23
goeatshit <43363087+goeatshit@users.noreply.github.com>
2019-04-23 23:10:00 +0800

    error & exception handling practice

--- Collection/src/com/Ian/abc/ProjectPractice.java
+++ Collection/src/com/Ian/abc/ProjectPractice.java
@@ -5,23 +5,25 @@ import java.util.Scanner;
 public class ProjectPractice {
 
 	public static void main(String[] args) {
-		Scanner c = new Scanner(System.in);
 		int av = 0;
-        try {
-		System.out.print("请输入数学成绩:");
-		int math = Integer.parseInt(c.nextLine());
-		System.out.print("请输入英文成绩:");
-		int english = Integer.parseInt(c.nextLine());
-	    av = (math+english)/2;
-        }catch(Exception w) {
-        	System.out.println("数值资料错误");
-        		
-        }
-		System.out.print("平均:"+av);
-		
-		
+		Scanner c = new Scanner(System.in);
+		try {
+			System.out.print("请输入数学成绩:");
+			int math = Integer.parseInt(c.nextLine());
+			System.out.print("请输入英文成绩:");
+			int english = Integer.parseInt(c.nextLine());
+			if (math >= 0 && math <= 100 && english >= 0 && english <= 100) {
+				av = (math + english) / 2;
+				System.out.print("平均:" + av);
+			} else if (math < 0 || math > 100 || english < 0 || english > 100
+					|| math < 0 && math > 100 && english < 0 && english > 100) {
+				System.out.println("你需要尊重，友愛，包起來!");
+			}
+		} catch (Exception w) {
+			System.out.println("数值资料错误");
+
+		}
 
 	}
 
-	
 }
